120e19eb7b83fbdafc7447e02dc720696e4882fc
RaulOZA <a1120150009@utch.edu.mx>
2024-09-18 20:19:21 -0600

    Se añade un else para que salga un mensaje para llenar el valor

--- app/src/main/java/com/example/edaddeperro/MainActivity.java
+++ app/src/main/java/com/example/edaddeperro/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +31,10 @@ public class MainActivity extends AppCompatActivity {
                    int resultado = edadint * 7;
                    String resultadostring = "La edad de tu peludo es: "+ resultado + "años";
                    resultexto.setText(resultadostring);
+               }
+               else
+               {
+                   Toast.makeText(MainActivity.this, "LLena la edad", Toast.LENGTH_LONG).show();
                }
             }
         });
